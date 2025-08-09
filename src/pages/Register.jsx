3d6d8a0e546b42579f76c33867c33c0b427562a6
Naeem Sarker <naeemsarker85@gmail.com>
2025-08-10 01:54:25 +0600

    feat: Setup Registration with Firebase

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import React, { useState } from "react";
+import { NavLink, useNavigate } from "react-router";
+import { registerWithEmailAndPassword } from "../firebase";
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+  const navigate = useNavigate();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
 
-export default Register
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const user = await registerWithEmailAndPassword(email, password);
+      console.log(user);
+      navigate("/login");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Register</h1>
+      <form>
+        <div>
+          <label htmlFor="email">Email Address</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+            placeholder="Email address"
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            placeholder="Password"
+          />
+        </div>
+        <button type="submit" onClick={handleSubmit}>
+          Register
+        </button>
+        <p>
+          Already Have an Accout?
+          <NavLink to="/login">Log in</NavLink>
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
